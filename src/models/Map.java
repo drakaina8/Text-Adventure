f49276915d2e4865843f9a41632835798961bf84
Hannah Hendrickson <hhendrickson8440@student.whatcom.edu>
2023-02-10 15:46:49 -0800

    Adding Map model.

---
+++ src/models/Map.java
@@ -0,0 +1,18 @@
+package src.models;
+
+public class Map {
+
+    public Map()
+    {
+
+    }
+
+    public static void createMap()
+    {
+        // TODO, create Roomlist of all rooms (JSON maybe)
+        // for (Room room : RoomList)
+        // {
+        //     Map.add(room);
+        // }
+    }
+}
