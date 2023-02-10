61fc4db53ec4fda86a288287f89f5259f87582e8
Hannah Hendrickson <hhendrickson8440@student.whatcom.edu>
2023-02-10 15:44:51 -0800

    Hannah & Patrick - Preliminary Map & Room model work

--- src/models/Room.java
+++ src/models/Room.java
@@ -1,4 +1,17 @@
 package src.models;
+
+import java.util.ArrayList;
+
 public class Room {
 
+    // Room object properties
+    int ID;
+    int roomNumber;
+    ArrayList<Object> items; // List of items in room
+    String blurb; // Description of room upon entry
+
+    public Room(int roomNumber, ArrayList<Object> items, String blurb)
+    {
+
+    }
 }
